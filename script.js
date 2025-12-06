5e8f4fb698542b408fd2185a09f43df6d8cf560e
Pedro Henrique Pereira <73310860+pedrohper@users.noreply.github.com>
2025-12-06 20:00:27 -0300

    alterações para db

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-const formulario = document.getElementById('formulario');
+const formulario = document.querySelector('form');
+const emailInput = document.querySelector('input[type="email"]');
+const passwordInput = document.querySelector('input[type="password"]');
+const cadastrarButton = document.getElementById('cadastrar');
+const entrarButton = document.getElementById('entrar');
 
-formulario.addEventListener('submit', function(event) {
+function cadastrarUsuario(email, senha) {
+    fetch('https://localhost:8080/cadastrar', {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }, 
+        method: 'POST',
+        body: JSON.stringify({ 
+            email: email, 
+            senha: senha })
+    })
+    .then(function(res) {console.log(res)})
+    .catch(function(res){console.log(res)});
+    console.log('Usuário cadastrado:', email, senha);
+}
+
+function limparFormulario() {
+    emailInput.value = '';
+    passwordInput.value = '';
+}
+
+cadastrarButton.addEventListener('click', function(event) {
     event.preventDefault();
-    alert('Formulário enviado com sucesso!');
-    formulario.reset();
+    cadastrarUsuario(emailInput.value, passwordInput.value);
+    limparFormulario();
+    window.location.replace('entrar.html');
 });
+
+entrarButton.addEventListener('click', function() {
+    window.location.replace('status.html');
+});
+
+
